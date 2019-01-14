f0ecaed5a974d36c1448a37838ab0e3e0f2f59f5
ninjasul@gmail.com <ninjasul@gmail.com>
2019-01-14 16:52:29 +0900

    videostore initial status

---
+++ videostore/src/main/java/Movie.java
@@ -0,0 +1,26 @@
+public class Movie {
+
+    public static final int CHILDRENS = 2;
+    public static final int NEW_RELEASE = 1;
+    public static final int REGULAR = 0;
+
+    private String title;
+    private int pricecode;
+
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        pricecode = priceCode;
+    }
+
+    public int getPriceCode() {
+        return pricecode;
+    }
+
+    public void setPriceCode(int arg) {
+        pricecode = arg;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
